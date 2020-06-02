5983a85525991742f5871390c3c83197d35756e3
Kieran Murray <kohyr@rocketmail.com>
2020-06-03 00:58:29 +0100

    Add Function To Find Valid Move For Royals

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const cardSuits = ["clubs", "spades", "hearts", "diamonds"];
 const numberOfJokers = 2;
 
-function numberedCardGridSlot () {
-    this.cards = [];
-    this.connectedRoyalCardGridSlots = [];
-  };
-  
+function numberedCardGridSlot() {
+  this.cards = [];
+  this.oppositeRoyalCardGridSlots = [];
+  this.adjacentRoyalGridSlots = [];
+}
+
+let royalCardGrid = [];
+
+let cardInHand;
+
 // Game Manager
 const gameManager = {
   state: "setup",
@@ -14,39 +19,123 @@ const gameManager = {
   generateNumberedCardGrid: function () {
     this.numberCardGrid[0] = new numberedCardGridSlot();
     this.numberCardGrid[0].connectedRoyalCardGridSlots = [4, 9];
+    this.numberCardGrid[0].adjacentRoyalGridSlots = [0, 3];
     this.numberCardGrid[1] = new numberedCardGridSlot();
     this.numberCardGrid[1].connectedRoyalCardGridSlots = [10];
+    this.numberCardGrid[1].adjacentRoyalGridSlots = [1];
     this.numberCardGrid[2] = new numberedCardGridSlot();
     this.numberCardGrid[2].connectedRoyalCardGridSlots = [3, 11];
+    this.numberCardGrid[2].adjacentRoyalGridSlots = [2, 4];
     this.numberCardGrid[3] = new numberedCardGridSlot();
     this.numberCardGrid[3].connectedRoyalCardGridSlots = [6];
+    this.numberCardGrid[3].connectedRoyalCardGridSlots = [5];
     this.numberCardGrid[4] = new numberedCardGridSlot();
+    this.numberCardGrid[4].connectedRoyalCardGridSlots = [];
     this.numberCardGrid[4].connectedRoyalCardGridSlots = [];
     this.numberCardGrid[5] = new numberedCardGridSlot();
     this.numberCardGrid[5].connectedRoyalCardGridSlots = [5];
+    this.numberCardGrid[5].adjacentRoyalGridSlots = [6];
     this.numberCardGrid[6] = new numberedCardGridSlot();
     this.numberCardGrid[6].connectedRoyalCardGridSlots = [0, 8];
+    this.numberCardGrid[6].connectedRoyalCardGridSlots = [7, 9];
     this.numberCardGrid[7] = new numberedCardGridSlot();
     this.numberCardGrid[7].connectedRoyalCardGridSlots = [1];
+    this.numberCardGrid[7].adjacentRoyalGridSlots = [10];
     this.numberCardGrid[8] = new numberedCardGridSlot();
     this.numberCardGrid[8].connectedRoyalCardGridSlots = [2, 7];
+    this.numberCardGrid[8].adjacentRoyalGridSlots = [8, 11];
   },
   populateNumberedCardGrid: function () {
-    for(let i = 0; i < this.numberCardGrid.length; i++)
-    {
-        while(this.numberCardGrid[i].cards.length === 0)
-        {
-            let cardToPlace = deck.drawCard();
-            if (cardToPlace.value === 0) {
-                jokerDeck.cards.push(cardToPlace);
-              } else if (cardToPlace.value === 1) {
-                acesDeck.cards.push(cardToPlace);
-              } else if (cardToPlace.value > 10) {
-                hand.cards.push(cardToPlace);
-              } else {
-                this.numberCardGrid[i].cards.push(cardToPlace);
-              }
+    for (let i = 0; i < this.numberCardGrid.length; i++) {
+      while (this.numberCardGrid[i].cards.length === 0) {
+        let cardToPlace = deck.drawCard();
+        if (cardToPlace.cardValue === 0) {
+          jokerDeck.cards.push(cardToPlace);
+        } else if (cardToPlace.cardValue === 1) {
+          acesDeck.cards.push(cardToPlace);
+        } else if (cardToPlace.cardValue > 10) {
+          hand.cards.push(cardToPlace);
+        } else {
+          this.numberCardGrid[i].cards.push(cardToPlace);
         }
+      }
+    }
+  },
+  populateRoyalCardGrid: function () {
+    for (let i = 0; i < hand.cards.length; i++) {
+      royalCardGrid.push(hand.cards[i]);
+    }
+    hand.cards = [];
+  },
+
+  findValidMoves: function () {
+    if (cardInHand) {
+      if (cardInHand.cardType === "royal") {
+        console.log(cardInHand);
+        console.log("Checking For Highest Value Of Matching Suit");
+        // find highest value card of same suit
+        let highestValueMatchingSlot = new numberedCardGridSlot();
+        for (let i = 0; i < this.numberCardGrid.length; i++) {
+          console.log(
+            `The suit of the slot is ${this.numberCardGrid[i].cards[0].suit}`
+          );
+          if (this.numberCardGrid[i].cards[0].suit === cardInHand.suit) {
+            console.log("The suit is a match");
+            if (highestValueMatchingSlot.cards.length > 0) {
+              if (
+                this.numberCardGrid[i].cards[0].cardValue >
+                highestValueMatchingSlot.cards[0].cardValue
+              ) {
+                highestValueMatchingSlot = this.numberCardGrid[i];
+              }
+            } else {
+              highestValueMatchingSlot = this.numberCardGrid[i];
+            }
+          }
+        }
+
+        if (highestValueMatchingSlot.cards.length === 0) {
+          console.log("Checking For Highest Value Colour");
+          // Check for hightest value card of the same colour
+          for (let i = 0; i < this.numberCardGrid.length; i++) {
+            if (
+              this.numberCardGrid[i].cards[0].cardColour ===
+              cardInHand.cardColour
+            ) {
+              if (highestValueMatchingSlot.cards.length > 0) {
+                if (
+                  this.numberCardGrid[i].cards[0].cardValue >
+                  highestValueMatchingSlot.cards[0].cardValue
+                ) {
+                  highestValueMatchingSlot = this.numberCardGrid[i];
+                }
+              } else {
+                highestValueMatchingSlot = this.numberCardGrid[i];
+              }
+            }
+          }
+        }
+
+        if (highestValueMatchingSlot.cards.length === 0) {
+          console.log("Checking For Highest Value of any colour");
+          // Check for hightest value card of any colour
+          for (let i = 0; i < this.numberCardGrid.length; i++) {
+            if (highestValueMatchingSlot.cards.length > 0) {
+              if (
+                this.numberCardGrid[i].cards[0].cardValue >
+                highestValueMatchingSlot.cards[0].cardValue
+              ) {
+                highestValueMatchingSlot = this.numberCardGrid[i];
+              }
+            } else {
+              highestValueMatchingSlot = this.numberCardGrid[i];
+            }
+          }
+        }
+
+        console.log(highestValueMatchingSlot);
+      } else if (cardInHand.cardType === "numbered") {
+      }
     }
   },
 };
@@ -61,7 +150,6 @@ let jokerDeck = {
 let acesDeck = {
   cards: [],
 };
-
 
 // Deck
 let deck = {
@@ -78,7 +166,7 @@ let deck = {
   },
   drawCard: function () {
     if (this.cards.length > 0) {
-        return this.cards.pop();
+      return this.cards.pop();
     } else {
       console.log("No cards left in deck");
     }
@@ -89,22 +177,27 @@ let deck = {
       for (let j = 1; j < 14; j++) {
         if (j === 1) {
           card = new aceCard();
-          card.value = j;
+          card.cardValue = j;
           card.suit = cardSuits[i];
           card.cardType = "ace";
         } else if (j > 1 && j < 11) {
           card = new numberCard();
-          card.value = j;
+          card.cardValue = j;
           card.suit = cardSuits[i];
           card.cardType = "numbered";
         } else {
           card = new royalCard();
-          card.value = j;
+          card.cardValue = j;
           card.health = j;
           card.suit = cardSuits[i];
           card.cardType = "royal";
         }
 
+        if (cardSuits[i] === "clubs" || cardSuits[i] === "spades") {
+          card.cardColour = "black";
+        } else {
+          card.cardColour = "red";
+        }
         this.cards.push(card);
       }
     }
@@ -121,9 +214,10 @@ let deck = {
 // Card Variables
 let generalCardProperties = {
   suit: "",
-  value: 0,
+  cardValue: 0,
   canBeMoved: "false",
   cardType: "",
+  cardColour: "",
 };
 let royalCardProperties = {
   armor: 0,
@@ -186,16 +280,21 @@ deck.initialise();
 deck.shuffle();
 gameManager.generateNumberedCardGrid();
 gameManager.populateNumberedCardGrid();
-console.log("Deck");
-console.log(deck.cards);
-console.log("Number Grid");
-console.log(gameManager.numberCardGrid);
-console.log("Hand");
-console.log(hand.cards);
-console.log("Aces")
-console.log(acesDeck.cards);
-console.log("Jokers");
-console.log(jokerDeck.cards);
+cardInHand = hand.cards[0];
+gameManager.findValidMoves();
+
+// console.log("Deck");
+// console.log(deck.cards);
+// console.log("Number Grid");
+// console.log(gameManager.numberCardGrid);
+// console.log("Royal Grid");
+// console.log(royalCardGrid);
+// console.log("Hand");
+// console.log(hand.cards);
+// console.log("Aces");
+// console.log(acesDeck.cards);
+// console.log("Jokers");
+// console.log(jokerDeck.cards);
 
 $(document).on("keypress", function (e) {
   if (String.fromCharCode(e.which) === "d") {
